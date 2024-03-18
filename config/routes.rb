54c1a055cc5c7494deeaee0401f627edc0637b65
xbr <berlianto.yusuf123@gmail.com>
2024-03-18 21:22:47 +0700

    change root to user

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root to: 'home#index'
+  root to: 'users#index'
 
   resources :users
   resources :daily_records
